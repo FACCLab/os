0cd826719aac98be73d1397df244eafd32401881
AngleNet <wangmin1123@iie.ac.cn>
2015-12-29 00:38:12 +0800

    Project0 initial commit

---
+++ p0/Makefile
@@ -0,0 +1,7 @@
+.PHONY: get_started update
+
+get_started:
+	./update.sh
+	$(MAKE)
+
+update: get_started
---
+++ p0/traceback/traceback.c
@@ -0,0 +1,23 @@
+/** @file traceback.c
+ *  @brief The traceback function
+ *
+ *  This file contains the traceback function for the traceback library
+ *
+ *  @author Harry Q. Bovik (hqbovik)
+ *  @bug Unimplemented
+ */
+
+#include "traceback_internal.h"
+
+void traceback(FILE *fp)
+{
+	/* the following just makes a sample access to "functions" array. 
+	 * note if "functions" is not referenced somewhere outside the 
+	 * file that it's declared in, symtabgen won't be able to find
+	 * the symbol. So be sure to always do something with functions */
+
+	/* remove this line once you've got real code here */
+	printf("first function in table name: %s\n", functions[0].name);
+}
+
+
