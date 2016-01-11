c8e1eee301d4d2503fabb6a60aabed346d6ddd2f
AngleNet <wangmin1123@iie.ac.cn>
2016-01-12 00:36:44 +0800

    Adding features

--- p0/tests/mytest.c
+++ p0/tests/mytest.c
@@ -4,11 +4,17 @@
 void first(){
 	traceback(stdout);
 }
-void second(){
+void second(int a, float b, double c, char d, char *e, char**f, void *g){
 	first();
 }
 void third(){
-	second();
+	int a = 8;
+	float b = 4.5;
+	double c = 3.3;
+	char d = 'd';
+	char *e = "argE";
+	const char *f[] = {"f1", "f2", "f3"};
+	second(a, b, c, d, e, (char **)f, (void *)&a);
 }
 void fourth(){
 	third();
--- p0/traceback/traceback.c
+++ p0/traceback/traceback.c
@@ -43,9 +43,9 @@ void print_func(FILE * fp, int ebp, int func_index){
 					break;
 				case TYPE_CHAR:
 					if(isprint(*(char *)(ebp+offset))){
-						fprintf(fp, "char %s=%c, ", name, *(char *)(ebp + offset));
+						fprintf(fp, "char %s='%c', ", name, *(char *)(ebp + offset));
 					}else{
-						fprintf(fp, "char %s=%o, ", name, *(char *)(ebp + offset));
+						fprintf(fp, "char %s='\\%o', ", name, *(char *)(ebp + offset));
 					}
 					break;
 				case TYPE_FLOAT:
@@ -54,6 +54,21 @@ void print_func(FILE * fp, int ebp, int func_index){
 				case TYPE_DOUBLE:
 					fprintf(fp, "double %s=%0.6f, ", name, *(double *)(ebp + offset));
 					break;
+				case TYPE_STRING:
+					fprintf(fp, "char *%s=\"%s\", ", name, (char *)(ebp + offset));
+					break;
+				case TYPE_STRING_ARRAY:
+					fprintf(fp, "char **%s=unimplemented, ", name);
+					break;
+				case TYPE_VOIDSTAR:
+					fprintf(fp, "void *%s=0v%x, ", name, ebp+offset);
+					break;
+				case TYPE_UNKNOWN:
+					fprintf(fp, "UNKNOWN %s=%p, ", name, (void *)(ebp+offset));
+					break;
+				default:
+					fprintf(fp, "\"Bug here\", ");
+
 			}
 			name = functions[func_index].args[i++].name;
 		}while(strlen(name) != 0);
